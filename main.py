e6b5db0a966cb71c2372a12e26a0dc015986fe14
Bingo <shrekthesaviour69@gmail.com>
2024-08-11 05:29:55 +0200

    Code simple proof of concept
    - Add main window for the app with static image
    - Display the window on screen bottom right corner
    - Test capabalities of Tkinter

--- main.py
+++ main.py
@@ -5,16 +5,28 @@ from PIL import Image, ImageTk
 root = tk.Tk()
 root.title("MAX")
 
-# Remove windows decorations
-root.overrideredirect(True)
-
-# Set transparency transparency and background color
-root.attributes('-transparentcolor', 'white')
-
 # Load assistant image
 image_path = "assets/images/mascot.png"
 image = Image.open(image_path)
 photo = ImageTk.PhotoImage(image)
+
+# Get screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+
+# Calculate poisition to place window in bottom right corner
+window_width = photo.width()
+window_height = photo.height()
+taskbar_height = 80
+
+x_position = screen_width - window_width
+y_position = screen_height- window_height - taskbar_height
+
+# Set the window size and poisition
+root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
+
+# Set transparency transparency and background color
+root.attributes('-transparentcolor', 'white')
 
 # Create a label to hold the image
 label = tk.Label(root, image=photo, bg='white')
